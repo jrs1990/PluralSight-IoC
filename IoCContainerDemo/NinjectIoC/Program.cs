6432c1bbd057c1411806b00611c689219a738b27
jansouza <jander.souza@b3.com.br>
2018-09-19 13:26:57 -0300

    commit de finalização do curso

--- IoCContainerDemo/NinjectIoC/Program.cs
+++ IoCContainerDemo/NinjectIoC/Program.cs
@@ -10,6 +10,18 @@ namespace NinjectIoC
     {
         static void Main(string[] args)
         {
+            var kernel = new StandardKernel();
+            kernel.Bind<ICreditCard>().To<MasterCard>().InSingletonScope();
+
+            var shopper = kernel.Get<Shopper>();
+            shopper.Charge();
+            Console.WriteLine(shopper.ChargesForCurrentCard);   
+            
+            var shopper2 = kernel.Get<Shopper>();
+            shopper2.Charge();
+            Console.WriteLine(shopper2.ChargesForCurrentCard);
+
+            Console.Read();
         }
     }
 }
